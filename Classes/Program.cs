3af0039a25298b7c601c91b7f86540cc2287fc50
Levi Williams <ljw758@hotmail.com>
2023-03-08 20:18:23 -0600

    Completed Exercise

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var silverado = new Car()
+            {
+                Model = "Silverado",
+                Make = "Chevrolet",
+                Year = 1
+            };
+            
+            Console.WriteLine($"Here is the selected car model: {silverado.Model} and its make: {silverado.Make} and it is {silverado.Year} year old");
         }
     }
 }
